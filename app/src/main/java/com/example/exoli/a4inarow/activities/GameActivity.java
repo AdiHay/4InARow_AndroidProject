0e4963bf8cbde2cf43480a83f7f56067b79447a8
Lior Rotberg <Liorr3@mail.afeka.ac.il>
2018-08-19 15:58:14 +0300

    GameMode

--- app/src/main/java/com/example/exoli/a4inarow/activities/GameActivity.java
+++ app/src/main/java/com/example/exoli/a4inarow/activities/GameActivity.java
@@ -5,9 +5,13 @@ import android.os.Bundle;
 
 import com.example.exoli.a4inarow.R;
 import com.example.exoli.a4inarow.classes.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DatabaseReference;
 
 public class GameActivity extends AppCompatActivity {
 
+    private FirebaseAuth mAuth;
+    private DatabaseReference db;
     private User user;
 
     @Override
